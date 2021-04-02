5b408d89126e482999e7035788215d02e65ac55e
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-04-02 18:28:27 -0300

    split header in components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,54 @@
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSideBarDrawer } from "../../contexts/SidebarDrawerContext";
+import { Logo } from "./Logo";
+import { NotificationsNav } from "./NotificationsNav";
+import { Profile } from "./Profile";
+import { Searchbox } from "./SearchBox";
+
+export default function Header() {
+    const { onOpen } = useSideBarDrawer()
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+    return (
+        <Flex
+            as="header"
+            w="100%"
+            maxW={1480}
+            h="20"
+            mx="auto"
+            mt="4"
+            px="6"
+            align="center"
+        >
+
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            )}
+
+            <Logo />
+
+            {isWideVersion &&
+                <Searchbox />
+            }
+
+            <Flex align="center" ml="auto">
+
+                <NotificationsNav />
+                <Profile showProfileData={isWideVersion} />
+
+            </Flex>
+        </Flex>
+    )
+}
