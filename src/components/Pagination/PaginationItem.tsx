7a5d3971e5a8ab98e33db59f5eb766626918af27
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-05-03 19:20:17 -0300

    create pagination component

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,8 +3,9 @@ import { Button } from "@chakra-ui/react";
 interface PaginationItem {
     number:number
     isCurrent?: boolean;
+    onPageChange: (page:number) => void
 }
-export function PaginationItem({ isCurrent = false, number }: PaginationItem) {
+export function PaginationItem({ isCurrent = false, number, onPageChange }: PaginationItem) {
     if (isCurrent) {
         return (
             <Button
@@ -31,6 +32,7 @@ export function PaginationItem({ isCurrent = false, number }: PaginationItem) {
             _hover={{
                 bg: 'gray.500'
             }}
+            onClick={() => onPageChange(number)}
         >{number}
         </Button>
     )
